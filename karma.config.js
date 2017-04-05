121842ee59d5e7d23cedde57b5676eec86f5aa58
echosteg <email@echosteg.com>
2017-04-05 20:40:04 +0530

    babel preprocessor for karma

--- karma.config.js
+++ karma.config.js
@@ -31,6 +31,7 @@ module.exports = function(config) {
       // preprocess matching files before serving them to the browser
       // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
       preprocessors: {
+          '**/*.spec.js':['babel']
       },
 
 
